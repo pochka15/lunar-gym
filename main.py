98fdb34fa0be2bc736bea7b13c5a7c271f076cae
Artem Kashkou <koshkov-artem@mail.ru>
2022-04-15 18:39:28 +0200

    Implement dumb action

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import gym
 
-DO_NOTHING_ACTION = 0
-FIRE_LEFT_ACTION = 1
-FIRE_MAIN_ACTION = 2
-FIRE_RIGHT_ACTION = 3
+IDLE_ACTION = 0
+LEFT_ACTION = 1
+UP_ACTION = 2
+RIGHT_ACTION = 3
+
+
+def create_dumb_action(x, current_step):
+    if current_step % 4 == 0:
+        return UP_ACTION
+    if current_step % 2 == 0:
+        return IDLE_ACTION
+    return LEFT_ACTION if x >= 0 else RIGHT_ACTION
+
 
 if __name__ == '__main__':
     # Configurations
@@ -16,8 +25,8 @@ if __name__ == '__main__':
         observation = env.reset()
         for t in range(steps_amount):
             env.render()
-            print(observation)
-            action = DO_NOTHING_ACTION
+            print(observation[0])
+            action = create_dumb_action(observation[0], t)
             observation, reward, done, info = env.step(action)
             print(f"Reward: {str(reward)}")
             if done:
